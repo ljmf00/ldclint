a1f871ca84a9c0f5a1b911d9b38282679d9946dc
Ilya Yanok <ilya.yanok@weka.io>
2024-11-01 12:32:41 +0100

    tests: fix overflow test on ARM64
    real == double on ARM64, so no warning is emitted.
    
    Signed-off-by: Ilya Yanok <ilya.yanok@weka.io>

--- tests/bad/overflow.d
+++ tests/bad/overflow.d
@@ -14,5 +14,10 @@ ulong mul(uint   val) { return val * 1234; }
 long  mul(int    val) { return val * -1234; }
 // CHECK-DAG: overflow.d(16): Warning: Expression `val * 1234.0F` may overflow before conversion
 real  mul(float  val) { return val * 1234.0f; }
-// CHECK-DAG: overflow.d(18): Warning: Expression `val * -1234.0` may overflow before conversion
-real  mul(double val) { return val * -1234.0; }
+static if (real.sizeof > double.sizeof) {
+    alias smaller_than_real = double;
+} else {
+    alias smaller_than_real = float;
+}
+// CHECK-DAG: overflow.d(23): Warning: Expression `val * -1234.0F` may overflow before conversion
+real  mul2(smaller_than_real val) { return val * -1234.0f; }
